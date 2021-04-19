575aa18bec1850788cfbef4774a874550827e4db
tmbaranova <tmb2508@gmail.com>
2021-04-19 15:34:07 +0300

    updater

--- my_arb_bot.py
+++ my_arb_bot.py
@@ -13,23 +13,25 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-class ArbitrBot:
 
+class ArbitrBot:
     def __init__(self):
         self.case_list = [1]
         self.bot = Bot(token=TELEGRAM_TOKEN)
         self.updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
         self.updater.dispatcher.add_handler(CommandHandler('del', self.delete_case_list))
         self.updater.dispatcher.add_handler(CommandHandler('show', self.show_case_list))
-        self.updater.dispatcher.add_handler(CommandHandler('upd', self.update_case_list))
+        self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.update_case_list))
 
         self.updater.start_polling()
 
-    def update_case_list(self, bot, update):
+    def update_case_list(self, update, context):
         print (type(update))
-        message = update.message
+        print (update)
+        message = update.message.text
         print (message)
         self.case_list.append(message)
+        context.bot.send_message(chat_id = update.message.chat.id, text= self.case_list)
         print (self.case_list)
         return self.case_list
 
