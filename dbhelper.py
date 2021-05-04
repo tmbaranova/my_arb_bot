99e50d678ac2c4d1e9ef9764d660220314ed2ebc
Баранова <tmb2508@gmail.com>
2021-05-04 11:23:07 +0300

    case_id+last_event_date

--- dbhelper.py
+++ dbhelper.py
@@ -1,12 +1,13 @@
 import os
 import psycopg2
+from datetime import datetime
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
 def create_connection():
     connection = None
     try:
-        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        connection = psycopg2.connect(DATABASE_URL)
         print("Connection to PostgreSQL DB successful")
     except psycopg2.OperationalError as e:
         print(f"The error '{e}' occurred")
@@ -17,15 +18,18 @@ def create_table():
     cur = conn.cursor()
     cur.execute(
         '''CREATE TABLE IF NOT EXISTS cases
-        (case_number TEXT);''')
+        (case_number TEXT,
+        case_id TEXT DEFAULT NULL,
+        last_event_date date);''')
     conn.commit()
     conn.close()
 
 def add_case(item_text):
     conn = create_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO cases (case_number) VALUES (%s)",
-                         (item_text,))
+    td = datetime.today()
+    sql = "INSERT INTO cases (case_number, case_id, last_event_date) VALUES (%s, %s, %s)"
+    cur.execute(sql, (item_text, None, td))
     conn.commit()
     conn.close()
 
